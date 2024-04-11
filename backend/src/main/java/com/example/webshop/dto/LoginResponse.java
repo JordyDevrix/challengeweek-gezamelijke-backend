b27e6fe875bea7fc96a8a2e0071b94d20ccaf5d8
Pieter van der Plas <pieter.vanderplas@wearejust.com>
2024-04-11 17:21:51 +0200

    added role to register and login requests

--- backend/src/main/java/com/example/webshop/dto/LoginResponse.java
+++ backend/src/main/java/com/example/webshop/dto/LoginResponse.java
@@ -1,11 +1,16 @@
 package com.example.webshop.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public class LoginResponse {
     public String email;
     public String token;
+    @JsonAlias("role_id")
+    public String role;
 
-    public LoginResponse(String email, String token) {
+    public LoginResponse(String email, String token, String role) {
         this.email = email;
         this.token = token;
+        this.role = role;
     }
 }

d7869840697607cfd027ecbc528cb7ebc7f761f2
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2024-04-11 13:27:17 +0200

    poging voor profile ts

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {GetOrder} from "../models/getOrder.model";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
+
 export class ProfileComponent {
+  public items: GetOrder[] = new Array<GetOrder>();
+  public orderId: number;
+  public total_amount:number=0;
+
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private ordersService: OrdersService
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.orderId = params['id'];
+    });
+
+    this.ordersService
+        .getOrderByIndex(this.orderId)
+        .subscribe((oitems: OrderItem[]) => {
+          this.items = oitems;
+          this.total_amount = oitems.reduce((n, {price}) => n + price, 0);
+        });
+  }
 
 }

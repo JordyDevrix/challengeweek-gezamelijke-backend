8774d9df0e589ccfce2272d20c944f9f6d9d805d
Rick Baller <rick@rbstudios.nl>
2024-04-11 16:19:20 +0200

    Fixes vor shit

--- frontend/src/app/pages/search-bar/search-bar.component.ts
+++ frontend/src/app/pages/search-bar/search-bar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {FaIconComponent, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  faMagnifyingGlass = faMagnifyingGlass;
+  constructor() { }
 
 }

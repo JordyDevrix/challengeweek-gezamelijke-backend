34cf904b1a08bf25b9afd17fd212c805f613abef
JordyDevrix <2109jordy2241@gmail.com>
2024-04-11 18:56:38 +0200

    Search by product brand/name is working now

--- frontend/src/app/pages/search-bar/search-bar.component.ts
+++ frontend/src/app/pages/search-bar/search-bar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import stringSimilarity from 'string-similarity';
 import {FaIconComponent, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {ProductsService} from "../../services/products.service";
+import {Product} from "../../models/product.model";
 
 @Component({
   selector: 'app-search-bar',
@@ -13,13 +15,19 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  productList: Product[] = [];
   myString = ""
   faMagnifyingGlass = faMagnifyingGlass;
-  constructor() { }
 
-  productList = [
-    "AKAI MPK mini mk3", "AKAI MPK mini mk2", "AKAI MPK mini MK1", "AKAI Mini plus", "AKAI mk3 plus", "AKAI MK2 plus", "AKAI LPK25", "Devine Ez creator", "M-Audio Keystation MK3", "Devine Ez creator plus", "M-Audio Keystation mini MK3", "AKAI Professional APC key", "Arturia Keylab MK3"
-  ];
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit()   {
+    this.productService
+      .getProducts()
+      .subscribe((productList: Product[]) => {
+        this.productList = productList;
+      });
+  }
 
 // Replace this with all the catogory's that exist
   categoryList = [
@@ -38,12 +46,34 @@ export class SearchBarComponent {
     console.log("Results by direct match:");
     for (let i = 0; i < this.productList.length; i++) {
       // Vergelijk de string (uit de zoekbalk) met elk product in de lijst van GET /api/pub/product/all
+      // console.log(`${this.productList[i].brand} ${this.productList[i].name}`)
       let ratio = 0;
-      ratio = this.stringSimilarity.compareTwoStrings(this.productList[i].toLowerCase(), this.myString.toLowerCase());
+
+      try {
+        ratio = this.stringSimilarity.compareTwoStrings(
+          `${this.productList[i].brand.toLowerCase()} ${this.productList[i].name.toLowerCase()}`,
+          this.myString.toLowerCase());
+
+      } catch (e) {
+        if (this.productList[i].brand == null) {
+          // console.log(`${this.productList[i].name.toLowerCase()}`)
+          ratio = this.stringSimilarity.compareTwoStrings(`${this.productList[i].name.toLowerCase()}`,
+            this.myString.toLowerCase());
+
+        } else if (this.productList[i].name == null) {
+          // console.log(`${this.productList[i].brand.toLowerCase()}`)
+          ratio = this.stringSimilarity.compareTwoStrings(`${this.productList[i].brand.toLowerCase()}`,
+            this.myString.toLowerCase());
+
+        } else {
+          console.log(e)
+        }
+      }
+
       if (ratio > 0.2) {
         // Als de ratio hoger is dan 0.2, print dan het product
         let product= "";
-        product = this.productList[i];
+        product = `${this.productList[i].brand} ${this.productList[i].name}`;
         // Voeg elk product toe aan de lijst van producten met een minimale ratio van 0.2
         productsFound.push(product);
         console.log(this.myString + "\t" + ratio + "\t" + product);
@@ -59,7 +89,7 @@ export class SearchBarComponent {
         // Hier ga je een request sturen voor alle categorieën die een ratio van 0.55 of hoger hebben
         // (E.G: GET /api/product/category/keyboards)
         for (const product of this.productList) {
-          if (productsFound.includes(product)) {
+          if (productsFound.includes(`${product.brand} ${product.name}`)) {
             continue;
           } else {
             // Hier kan je elk product uit de betreffende categorie printen die je van te voren hebt aangeroep op een get category endpoint
